aea7765a2f8f486fa4da20c0e70eeca5492fd143
Niall Wallace <nj.wallace@outlook.com>
2015-08-30 18:20:24 +0100

    getting there....

--- ruby_fun/questions.rb
+++ ruby_fun/questions.rb
@@ -93,35 +93,54 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
-  
+  shortest_word_length = 10
+  shortest_word = ''
+  array.select { |word|
+    if word.length < shortest_word_length
+      shortest_word = word
+      shortest_word_length = word.length
+    end
+  }
+  return shortest_word
 end
 
 # return the longest word in an array
 def longest_word_in_array(array)
-  
+    shortest_word_length = 0
+  shortest_word = ''
+  array.select { |word|
+    if word.length > shortest_word_length
+      shortest_word = word
+      shortest_word_length = word.length
+    end
+  }
+  return shortest_word
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.inject{|sum,x| sum + x }
   
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  #array.select { |num| array.push(num) }
   
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
   
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
-  
+  array.inject { |sum, var| sum + var } / array.length.to_i + 1
 end
 
 # get all the elements in an array, up until the first element
@@ -129,7 +148,16 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
-  
+#   x = 0
+#   new_array = []
+#   while true
+#    array.map { |num| new_array.push(num)
+#     if num >= 5
+#       break
+#     end
+#   }
+# end
+#   return new_array
 end
 
 # turn an array (with an even number of elements) into a hash, by
@@ -144,51 +172,56 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
-  
+  array.map! {|value| value.split('')}
+  array.flatten.uniq.sort
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
-  
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
-  
+  new_array = hash.flatten
+  new_array.inject { |total, num| total + num}
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
-  
+
 end
 
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
-  
+  float = float + 1
+  float.to_i
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+    float.to_i
   
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
-  
+
 end
 
 # get the domain name *without* the .ly (or .com, .co etc) part, from an email address
 # so jeremy@generalassemb.ly becomes generalassemb.ly
 def get_domain_name_from_email_address(email)
-  
+ email_array = email.split('@')
+ domain = email_array[1].to_s
 end
 
 # capitalize the first letter in each word of a string, 
@@ -197,7 +230,7 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
-
+  string.split(" ").map{|word| word.capitalize }.join(" ")
 end
 
 # return true if a string contains any special characters
@@ -221,6 +254,7 @@ end
 
 # get the square root of a number
 def square_root_of(number)
+  Math.sqrt(number)
   
 end
 
